87c6a13f299f1c063692eaa41cb7671b750c9b69
ryszard <japan26@interia.pl>
2020-07-05 01:27:16 +0200

    Innitial commit first test

---
+++ src/com/company/Main.java
@@ -0,0 +1,12 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner skaner = new Scanner(System.in);
+        byte wiek = skaner.nextByte();
+        System.out.println("Masz "+wiek+" lat");
+    }
+}
